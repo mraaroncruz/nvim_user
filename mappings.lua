920146936aadee74ba32d6b2cc4b36eb6e3cd6b2
Aaron Cruz <pferdefleisch+git@gmail.com>
2023-08-14 15:08:11 +0200

    Add some mappings and favorite plugins

--- mappings.lua
+++ mappings.lua
@@ -32,6 +32,8 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-p>"] = { function() require("telescope.builtin").find_files() end, desc = "Find files" },
+    [",,"] = { "<C-^>" }
   },
   t = {
     -- setting a mapping to false will disable it
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,40 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  { "tpope/vim-fugitive", event = "VeryLazy" },
+
+  { "mg979/vim-visual-multi", event = "VeryLazy" },
+
+  { "windwp/nvim-ts-autotag",
+    autotag = {
+      enable = true
+    }
+  },
+
+  {
+    "rose-pine/neovim",
+    as = "rose-pine",
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+    end,
+    event = "VeryLazy"
+  },
+
+  { "folke/tokyonight.nvim", 
+    lazy = false,
+    as = "tokyonight",
+    config = function()
+      vim.cmd("colorscheme tokyonight")
+    end
+  },
+
+  {
+    "drewtempelmeyer/palenight.vim",
+    as = "palenight",
+    config = function()
+      vim.cmd("colorscheme palenight")
+    end
+  },
+
+  "github/copilot.vim"
 }
