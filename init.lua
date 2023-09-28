1222fbb1f6172a57fac5946a7cd5532dee582e37
Aaron Cruz <pferdefleisch+git@gmail.com>
2023-09-28 16:22:32 +0200

    Fix vscode snippets

--- init.lua
+++ init.lua
@@ -68,24 +68,6 @@ return {
     },
   },
 
-	-- Configure plugins
-  plugins = {
-    {
-      "L3MON4D3/LuaSnip",
-      config = function(plugin, opts)
-        -- include the default astronvim config that calls the setup call
-        require "plugins.configs.luasnip"(plugin, opts)
-        -- load snippets paths
-        require("luasnip.loaders.from_vscode").lazy_load {
-          -- this can be used if your configuration lives in ~/.config/nvim
-          -- if your configuration lives in ~/.config/astronvim, the full path
-          -- must be specified in the next line
-          paths = { "./lua/user/snippets" }
-        }
-      end,
-    },
-  },
-
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
